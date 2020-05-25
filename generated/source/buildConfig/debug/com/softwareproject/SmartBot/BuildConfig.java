f9f0e7737f703471b77d55485f20b95132648448
avanthikaD <64132328+avanthikaD@users.noreply.github.com>
2020-05-26 00:23:56 +0530

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/softwareproject/SmartBot/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.softwareproject.SmartBot;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.softwareproject.SmartBot";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
